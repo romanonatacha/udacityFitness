c0a0d11906fbfe671bbb014fc8833e61d0f2e751
Natacha Romano <natacharomanonr@gmail.com>
2019-05-15 20:14:39 -0300

    finish navigation

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -9,7 +9,8 @@ import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions';
-import { white, purple } from '../utils/colors';
+import { white, purple } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -78,7 +79,7 @@ class AddEntry extends Component {
             eat: 0
         }))
 
-        // navigate to home
+        this.toHome()
 
         submitEntry({ key, entry })
 
@@ -92,9 +93,15 @@ class AddEntry extends Component {
             [key]: getDailyReminderValue()
         }))
 
-        // route to home
+        this.toHome()
 
         removeEntry(key)
+    }
+
+    toHome = () => {
+        this.props.navigation.dispatch(NavigationActions.back({
+            key: 'AddEntry'
+        }))
     }
 
     render() {
