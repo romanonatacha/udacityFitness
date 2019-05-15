e1a04c00353d2492bdc8da8d44a10dade12ca40f
Natacha Romano <natacharomanonr@gmail.com>
2019-05-15 18:05:23 -0300

    routing part 1

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { View, Platform, StatusBar } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-import { createBottomTabNavigator, createMaterialTopTabNavigator, createAppContainer } from 'react-navigation'
+import {
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator,
+  createAppContainer,
+  createStackNavigator
+  } from 'react-navigation'
 import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
+import EntryDetail from './components/EntryDetail'
 
 function UdaciStatusBar ({ backgroundColor, ...props }) {
   return (
@@ -55,19 +61,38 @@ const tabNavigatorConfig = {
   }
 }
 
+const MainNavigator = createAppContainer(
+  createStackNavigator({
+    Home: {
+      screen: Tabs,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    EntryDetail: {
+      screen: EntryDetail,
+      navigationOptions: {
+        headerTintColor: white,
+        headerStyle: {
+          backgroundColor: purple
+        }
+      }
+    }
+  })
+)
+
 const Tabs = Platform.OS === 'ios'
   ? createBottomTabNavigator(RouteConfigs, tabNavigatorConfig)
   : createMaterialTopTabNavigator(RouteConfigs, tabNavigatorConfig)
 
-const TabsContainer = createAppContainer(Tabs)
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
           <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
-            <TabsContainer />
+            <MainNavigator />
         </View>
       </Provider>
     )
--- components/History.js
+++ components/History.js
@@ -9,6 +9,7 @@ import { white } from '../utils/colors'
 import DateHeader from './DateHeader'
 import MetricCard from './MetricCard'
 import { AppLoading } from 'expo'
+import EntryDetail from './EntryDetail';
 
 class History extends Component {
     state = {
@@ -38,7 +39,10 @@ class History extends Component {
                         {today}
                     </Text>
                 </View>
-                : <TouchableOpacity onPress={() => console.log('pressed')}>
+                : <TouchableOpacity onPress={() => this.props.navigation.navigate(
+                    'EntryDetail',
+                    {  entryId: key }
+                )}>
                     <MetricCard metrics={metrics} date={formattedDate} />
                 </TouchableOpacity>
             }
